78e9964044d8143f59295b879c94ad4369c2dd09
Wallace-2007 <wallacerodrigues689@gmail.com>
2024-11-29 08:58:34 -0300

    ULTIMA DESSA VEZ

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -69,12 +69,12 @@ const Collection = () => {
       {/* Filtros */}
       <div className='min-w-60'>
         <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>
-          FILTERS
+          FILTROS
           <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
         </p>
         {/* Filtro de Categoria */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
-          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <p className='mb-3 text-sm font-medium'>CATEGORIAS</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
               <input className='w-3' type="checkbox" value='Men' onChange={toggleCategory} />Homem
@@ -89,7 +89,7 @@ const Collection = () => {
         </div>
         {/* Filtro de SubCategoria */}
         <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block`}>
-          <p className='mb-3 text-sm font-medium'>TYPE</p>
+          <p className='mb-3 text-sm font-medium'>TIPOS</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
               <input className='w-3' type="checkbox" value='Topwear' onChange={toggleSubCategory} />Roupas de cima
@@ -107,7 +107,7 @@ const Collection = () => {
       {/* Produtos */}
       <div className='flex-1'>
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
-          <Title text1='ALL' text2='COLLECTIONS' />
+          <Title text1='NOSSAS' text2='COLEÇÕES' />
           {/* Ordenação */}
           <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
             <option value='relevant'>Ordenar por: Relevância</option>
