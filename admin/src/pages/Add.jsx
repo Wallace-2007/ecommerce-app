3dfb21f86bd410f0280c813c113da85430e3fb2a
Wallace-2007 <wallacerodrigues689@gmail.com>
2024-11-29 11:10:24 -0300

    outro

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -94,7 +94,7 @@ const Add = ({token}) => {
         <input onChange={(e)=>setName(e.target.value)} value={name} className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required/>
       </div>
       <div className='w-full'>
-        <p className='mb-2'>Product description</p>
+        <p className='mb-2'>Descrição do produto</p>
         <textarea onChange={(e)=>setDescription(e.target.value)} value={description} className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required/>
       </div>
 
